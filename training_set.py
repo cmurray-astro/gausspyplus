aa4bda402719d079ed5ed03d87f577ba500b395b
mriener <riener@mpia.de>
2019-03-17 15:04:40 +0100

    bugfixes in training_set and plotting

--- training_set.py
+++ training_set.py
@@ -2,7 +2,7 @@
 # @Date:   2019-02-18T16:27:12+01:00
 # @Filename: training_set.py
 # @Last modified by:   riener
-# @Last modified time: 2019-03-16T11:52:03+01:00
+# @Last modified time: 2019-03-17T15:04:19+01:00
 
 import ast
 import configparser
@@ -108,7 +108,6 @@ class GaussPyTrainingSet(object):
             self.get_values_from_config_file(configFile)
 
     def get_values_from_config_file(self, configFile):
-        print('works')
         config = configparser.ConfigParser()
         config.read(configFile)
 
@@ -184,7 +183,7 @@ class GaussPyTrainingSet(object):
 
         #  start multiprocessing
         mp_init([self, indices])
-        results_list = mp_func(self.n_spectra)
+        results_list = mp_func(self.n_spectra, use_ncpus=self.use_ncpus)
         print('SUCCESS\n')
         for result in results_list:
             if result is not None:
